66dfc524e0037dc51fb54403f83635277175715f
gauravyadav199623 <gauravyadav199623@gmail.com>
2023-12-17 14:56:30 +0530

    bcrypt

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const sequelize = require('./util/database');
 const app = express();
 const ExpenseUser=require('./models/expense-user')
 var cors=require('cors')
+const bcrypt = require('bcrypt');
 app.use(cors())
 
 app.use(bodyParser.json({ extended: false }));
+const saltRounds = 10;
 
 
 
@@ -20,14 +22,18 @@ app.post('/post-expense', async(req,res,next)=>{
     const name=req.body.name;
     const email=req.body.email;
     const password=req.body.password; 
-    const data=await ExpenseUser.create({name:name, email:email, password:password});
-    // const userEmail = await ExpenseUser.findOne({ where: { userEmail: email } });
-    // if(!userEmail){
-    //     res.status(201).json({newUser:data})
-    // }else{
-    //     res.status(404).json({error:"USER Already exist"})
-    // }
-    res.status(201).json({expenseAdded:data})
+    bcrypt.hash(password,saltRounds, async(err,hash)=>{
+
+        const data=await ExpenseUser.create({name:name, email:email, password:password});
+        // const userEmail = await ExpenseUser.findOne({ where: { userEmail: email } });
+        // if(!userEmail){
+        //     res.status(201).json({newUser:data})
+        // }else{
+        //     res.status(404).json({error:"USER Already exist"})
+        // }
+        res.status(201).json({expenseAdded:data, redirect: '/login'})
+        // res.redirect('/login')
+    })
 })
 app.post('/login',async(req,res,next)=>{
     console.log(JSON.stringify(req.body)+"123456")
@@ -37,14 +43,18 @@ app.post('/login',async(req,res,next)=>{
     // const data=await ExpenseUser.create({ email:email, password:password});
 
     let user=await ExpenseUser.findOne({where:{email:email}})
-    if (user) {
-        let pass=await ExpenseUser.findOne({where:{password:password}})
-        if(pass){
-            res.status(200).json({  message: 'User login successfully' });
-            // res.render('dashboard', { message: 'User login successfully' });
-        }else{
-            res.status(401).json({message:"User not authorized"})
-        }
+    // let userData=user.toJSON()
+    // console.log(JSON.stringify(userData)+"user Data");
+    if (user){
+        console.log(JSON.stringify(user.dataValues)+"<-User");
+        bcrypt.compare(password,user.password,(err,result)=>{
+            if(result==true){
+                res.status(200).json({  message: 'User login successfully' });
+                // res.render('dashboard', { message: 'User login successfully' });
+            }else{
+                res.status(401).json({message:"User not authorized"})
+            }
+        })
       } else {
         res.status(404).json({ message: "user not found" });
       }
