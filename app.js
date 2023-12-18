52b30348881d05b26309bb237146cb9dd887707d
gauravyadav199623 <gauravyadav199623@gmail.com>
2023-12-18 16:18:07 +0530

    foreign key

--- app.js
+++ app.js
@@ -3,21 +3,27 @@ const path=require('path');
 const express=require('express');
 const bodyParser=require('body-parser');
 const sequelize = require('./util/database');
+
 const jwt = require('jsonwebtoken');
 
 const app = express();
+
 const ExpenseUser=require('./models/user');
 const ExpenseItem=require('./models/expenses');
+
 var cors=require('cors')
 const bcrypt = require('bcrypt');
-const Expense = require('./models/expenses');
-const User = require('./models/user');
+// const Expense = require('./models/expenses');
+// const User = require('./models/user');
+
 app.use(cors())
 
 app.use(bodyParser.json({ extended: false }));
 const saltRounds = 10;
 
-
+function generateAccessToken(id,name){
+    return jwt.sign({userId:id, name:name},'secreteKey')
+}
 
 
 
@@ -56,7 +62,7 @@ app.post('/login',async(req,res,next)=>{
         console.log(JSON.stringify(user.dataValues)+"<-User");
         bcrypt.compare(password,user.password,(err,result)=>{
             if(result==true){
-                res.status(200).json({  message: 'User login successfully' });
+                res.status(200).json({  message: 'User login successfully',token: generateAccessToken(user.id, user.name)});
                 // res.render('dashboard', { message: 'User login successfully' });
             }else{
                 res.status(401).json({message:"User not authorized"})
@@ -67,19 +73,47 @@ app.post('/login',async(req,res,next)=>{
       }
 })
 
+
+app.post('/expense/post-expense',(req,res,next)=>{
+    const token=req.header('Authorization');
+    console.log(token);
+    const user=jwt.verify(token,'secreteKey');
+    console.log("userID>>>>>>",user.userId)
+    ExpenseUser.findByPk(user.userId).then(user=>{
+        console.log(JSON.stringify(user));
+        req.user=user; //kim
+        next();
+    })   
+})
 app.post('/expense/post-expense',async(req,res,next)=>{
     const amount=req.body.amount;
     const description=req.body.description;
     const category=req.body.category;
+    const id=req.user.id
+    console.log(id)
     const data=await ExpenseItem.create({
-        amount:amount,description:description,category:category
+        amount:amount,description:description,category:category, userId:id
     });
     res.status(201).json({expenseAdded:data})
 })
+
+app.get('/expense/get-expenses',(req,res,next)=>{
+    const token=req.header('Authorization');
+    console.log(token);
+    const user=jwt.verify(token,'secreteKey');
+    console.log("userID>>>>>>",user.userId)
+    ExpenseUser.findByPk(user.userId).then(user=>{
+        console.log(JSON.stringify(user));
+        req.user=user; //kim
+        next();
+    })   
+})
 app.get('/expense/get-expenses',async(req,res,next)=>{
-    const expenses=await ExpenseItem.findAll();
+    const expenses=await ExpenseItem.findAll({where:{userId:req.user.id}});
     res.status(200).json({allExpenses:expenses})
 })
+
+
 app.delete('/expense/delete-expense/:id',async(req,res,next)=>{
     const id=req.params.id;
     console.log(id)
