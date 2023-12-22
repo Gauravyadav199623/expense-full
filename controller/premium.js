894956ebc60064aaaada945642f6165f6368eb99
gauravyadav199623 <gauravyadav199623@gmail.com>
2023-12-22 16:45:29 +0530

    opt1b

--- controller/premium.js
+++ controller/premium.js
@@ -6,30 +6,18 @@ console.log('hello premium');
 
 const getUser=async(req,res,next)=>{
     try{
-        const users=await User.findAll({
-            attributes:['id','name']
+        const userLeaderBoardDetails=await User.findAll({
+            attributes:['id','name',[sequelize.fn('sum',sequelize.col('expenses.amount')),'totalCost']],
+            include:[
+                {
+                    model:Expense,
+                    attributes:[]
+                }
+            ],
+            group:['user.id'],
+            order:[['totalCost','DESC']]
         })
-        const expenses=await Expense.findAll({
-            attributes:['userId',[sequelize.fn('sum',sequelize.col('expense.amount')),'totalCost']],
-            group:['userId']
-        })
-        const individualSum={}
-        console.log(expenses,'expensesssssssssssssssssss')
-        console.log(users,'userssssssssssssssssssssss')
-        expenses.forEach(expense => {
-            console.log(expense.amount)
-            if(individualSum[expense.userId]){
-                individualSum[expense.userId]+=expense.amount
-            }else{
-                individualSum[expense.userId]=expense.amount
-            }
-        });
-        let userLeaderBoardDetails=[];
-        users.forEach(user=>{
-            userLeaderBoardDetails.push({name:user.name,totalCost:individualSum[user.id] || 0})
-        })
-        console.log(userLeaderBoardDetails,'userLeaderBoardDetails')
-        userLeaderBoardDetails.sort((a,b)=>b.totalCost-a.totalCost)
+
         res.status(200).json(userLeaderBoardDetails)
 
     }catch(err){
