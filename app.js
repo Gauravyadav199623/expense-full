117a42af91353504c1779b3a68176c3b601cdd9d
gauravyadav199623 <gauravyadav199623@gmail.com>
2023-12-27 16:05:09 +0530

    yep

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const path=require('path');
+const fs=require('fs')
 
 const express=require('express');
 const bodyParser=require('body-parser');
 const sequelize = require('./util/database');
+const helmet=require('helmet');
+const morgan=require('morgan');
 
 
 
@@ -11,6 +14,8 @@ const app = express();
 const User=require('./models/user');
 const ExpenseItem=require('./models/expenses');
 const Order=require('./models/orders');
+const forgotPassword=require('./models/forget-password')
+const DownloadFile=require('./models/downloaded-files')
 
 const userRoutes=require('./routes/user')
 const expensesRoutes=require('./routes/expenses')
@@ -18,9 +23,14 @@ const purchaseRoutes = require('./routes/purchase');
 const premiumRoutes=require('./routes/premium')
 const PasswordRoutes=require('./routes/password')
 
+const accessLogStream=fs.createWriteStream(path.join(__dirname, 'access.log'),{flags:'a'});
+
+
 var cors=require('cors')
 
+app.use(helmet())
 app.use(cors())
+app.use(morgan('combined',{stream:accessLogStream}))
 
 
 app.use(bodyParser.json({ extended: false }));
@@ -39,6 +49,13 @@ ExpenseItem.belongsTo(User)
 
 User.hasMany(Order);
 Order.belongsTo(User)
+
+
+User.hasMany(forgotPassword);
+forgotPassword.belongsTo(User)
+
+User.hasMany(DownloadFile);
+DownloadFile.belongsTo(User);
 
 
 sequelize
